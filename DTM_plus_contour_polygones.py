1d1471bc6d317b2e3396dbfd541a1a6568be9f3f
Polymerizable <binarydetritus@protonmail.com>
2024-04-24 21:55:26 +0200

    geopandas package
    - installed packages
    - testing how to read the .asc files

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import geopandas as gpd
+import numpy as np
+import rasterio as rio
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 
-# read the .asc fies that contain the DTM raster info
-df = gpd.read_file('EGM722_Project_Data/DTM_asc/TL4358.asc')
-df.head(5)
 
+# function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
+gdf = rio.read_file('EGM722_Project_Data/DTM_asc/TL4358.asc')
+gdf.head(5)
+
+gdf = gdf.set_crs("EPSG:27700")  # this sets the coordinate reference system to epsg:27700, wgs84 lat/lon
