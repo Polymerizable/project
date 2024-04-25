8939e4167cf5c608a3e4869ccb55202643089b28
Polymerizable <binarydetritus@protonmail.com>
2024-04-25 18:33:11 +0200

    Leaving crs change code for futue

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -11,7 +11,9 @@ british_national_grid = CRS.from_epsg(27700)
 
 # Setting a DatasetReader object for reading the dataset and its attributes
 dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
-# Setting a new CRS as 'British National Grid'='EPSG:27700'
-dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
 dtm = dataset.read()
+
+
+# Setting a new CRS as 'British National Grid'='EPSG:27700'
+# dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
