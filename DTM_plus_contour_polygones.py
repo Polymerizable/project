6764a94b44af6cff117644e89b9733991de7222d
Polymerizable <binarydetritus@protonmail.com>
2024-04-25 22:10:08 +0200

    Define the path of the file

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -9,10 +9,16 @@ from pyproj import CRS
 # Define the British National Grid CRS (EPSG:27700)
 british_national_grid = CRS.from_epsg(27700)
 
+# Define the path of the file
+asc_file_path = 'D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc'
+
 # Setting a DatasetReader object for reading the dataset and its attributes
 dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
 dtm = dataset.read()
+
+print(dtm.crs)
+
 
 
 # Setting a new CRS as 'British National Grid'='EPSG:27700'
