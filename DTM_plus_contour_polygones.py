febe7c4db30f0350a7094d9022b02c1d51275724
Polymerizable <binarydetritus@protonmail.com>
2024-04-25 11:49:27 +0200

    Reading .asc file through a dataset variable

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -14,7 +14,7 @@ dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM7
 # Setting a new CRS as 'British National Grid'='EPSG:27700'
 dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
-dtm = rio.read_file('EGM722_Project_Data/DTM_asc/TL4358.asc')
+dtm = dataset.read()
 
 # Set the CRS to the dataset (two ways for doing it)
 dtm = gdf.set_crs(british_national_grid) is a second way to set
