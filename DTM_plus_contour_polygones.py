252620e676ad5f9d9fffced5ed2d955c16e1e168
Polymerizable <binarydetritus@protonmail.com>
2024-04-25 11:42:26 +0200

    Reading directly .asc file

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -9,8 +9,6 @@ from pyproj import CRS
 # Define the British National Grid CRS (EPSG:27700)
 british_national_grid = CRS.from_epsg(27700)
 
-# Setting a DatasetReader object for reading the dataset and its attributes
-dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
 # Setting a new CRS as 'British National Grid'='EPSG:27700'
 dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
