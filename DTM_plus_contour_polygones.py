5acfd5597a2dc37093236d7a69c6d9d5e11ade95
Polymerizable <binarydetritus@protonmail.com>
2024-04-25 11:39:57 +0200

    Reading raster
    - reading raster file with rasterio
    - setting the CRS as it has None

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -9,10 +9,13 @@ from pyproj import CRS
 # Define the British National Grid CRS (EPSG:27700)
 british_national_grid = CRS.from_epsg(27700)
 
+# Setting a DatasetReader object for reading the dataset and its attributes
+dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
+# Setting a new CRS as 'British National Grid'='EPSG:27700'
+dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
-gdf = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
-gdf = rio.read_file('EGM722_Project_Data/DTM_asc/TL4358.asc')
+dtm = rio.read_file('EGM722_Project_Data/DTM_asc/TL4358.asc')
 
 # Set the CRS to the dataset (two ways for doing it)
-gdf = gdf.set_crs(british_national_grid) is a second way to set
-gdf = gdf.set_crs("EPSG:27700")  # this sets the coordinate reference system to epsg:27700, wgs84 lat/lon
+dtm = gdf.set_crs(british_national_grid) is a second way to set
+dtm = gdf.set_crs("EPSG:27700")  # this sets the coordinate reference system to epsg:27700, wgs84 lat/lon
