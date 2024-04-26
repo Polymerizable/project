8d163742535cea67776e199c97706c4955a040a4
Polymerizable <binarydetritus@protonmail.com>
2024-04-26 05:51:29 +0200

    setting the raster path and open it

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -13,7 +13,11 @@ british_national_grid = CRS.from_epsg(27700)
 asc_file_path = 'D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc'
 
 # Open the .asc file
-with rasterio.open(asc_file_path) as dataset:
+with rio.open(asc_file_path) as dataset:
+
+    # Set the coordinate reference system to British National Grid (EPSG:27700)
+    #dataset.crs = rasterio.crs.CRS({'init': 'epsg:27700'})
+
 
 # Setting a DatasetReader object for reading the dataset and its attributes
 dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
