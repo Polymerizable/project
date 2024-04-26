4879b0a99a108afb3d9bfc61626907ba1f632fd6
Polymerizable <binarydetritus@protonmail.com>
2024-04-26 11:03:45 +0200

    Defining a lis of the files present in the working folder Set a loop to work with each file of the folder

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -5,28 +5,40 @@ import rasterio as rio
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 from pyproj import CRS
+import os
 
-# Define the CRS, in this case British National Grid CRS (EPSG:27700)
+# Define the CRS, in this case British National Grid (EPSG:27700)
 british_national_grid = CRS.from_epsg(27700)
+
+# Define folder path
+folder_path = 'D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc'
+
+# Create a list with all the files in the folder
+files = os.listdir(folder_path)
+
+# Iterate through each file in the folder to work with them
+for file_name in files:
 
 # Define the path of the file
 asc_file_path = 'D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc'
 
-# Open the .asc file
-with rio.open(asc_file_path, crs=british_national_grid) as dataset:
-
-    # Set the coordinate reference system to British National Grid (EPSG:27700)
-    #dataset.crs = rasterio.crs.CRS({'init': 'epsg:27700'})
-
-
 # Setting a DatasetReader object for reading the dataset and its attributes
-dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
+dataset = rio.open(asc_file_path)
+
+if dataset.crs is None:
+    new_crs = imput(f"CRS is not specified for '{file_name}'. Please specify a CRS (e.g., 'EPSG:27700'): ")
+
+
+with rio.open(asc_file_path, mode="r+", crs="british_national_grid") as new_dataset:
+    print(new_dataset.profile)
+
+print(dataset.profile)
+print(new_dataset.profile)
+print(dataset.crs)
+
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
-dtm = dataset.read()
-
-print(dtm.crs)
+#dtm = dataset.read()
 
 
-
-# Setting a new CRS as 'British National Grid'='EPSG:27700'
-# dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
+dataset.close()
+new_dataset.close()
