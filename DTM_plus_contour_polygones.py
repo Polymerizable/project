93ea5711c2ccae01f51de42d705fbe43bc7bd34a
Polymerizable <binarydetritus@protonmail.com>
2024-04-26 12:31:12 +0200

    Asking for a new crs if there is No one, and set it

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -12,21 +12,26 @@ british_national_grid = CRS.from_epsg(27700)
 # Define the path of the file
 asc_file_path = 'D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc'
 
-# Open the .asc file
-with rio.open(asc_file_path, crs=british_national_grid) as dataset:
-
-    # Set the coordinate reference system to British National Grid (EPSG:27700)
-    #dataset.crs = rasterio.crs.CRS({'init': 'epsg:27700'})
-
-
 # Setting a DatasetReader object for reading the dataset and its attributes
-dataset = rio.open('D:/Ulster/EGM722_Programming_for_GIS_and_Remote_Sensing/EGM722_Assesment/EGM722_Project/project/EGM722_Project_Data/DTM_asc/TL4358.asc')
+with rio.open(asc_file_path) as dataset:
+    if dataset.crs is None:
+        # Ask the user to specify the Coordinate Reference System (CRS)
+        new_crs = input(f"CRS is not specified for '{dataset.name}'. Please specify a CRS (e.g., 'EPSG:27700'): ")
+        print(f"The specified crs for the file is {new_crs}")
+
+        # Update the dataset's profile with the new CRS
+        profile = dataset.profile
+        profile['crs'] = new_crs
+
+        # Read the data
+        data = dataset.read()
+
+        # Write the data with the updated profile
+        with rio.open('new_raster.asc', 'w', **profile) as dst:
+            dst= rio.write(data)
+    else:
+        print(f"CRS is specified for '{dataset.name}': {dataset.crs}")
+
 # function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
-dtm = dataset.read()
-
-print(dtm.crs)
-
-
-
-# Setting a new CRS as 'British National Grid'='EPSG:27700'
-# dataset.crs = rio.crs.CRS({'init': 'epsg:27700'})
+#dtm = dataset.read('crs':)
+#print(dtm.crs)
