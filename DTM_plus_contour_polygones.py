01c608ee8041fd40927ecce190bd92afa2ade3e6
Polymerizable <binarydetritus@protonmail.com>
2024-04-28 22:56:38 +0200

    deleted extra code form before

--- DTM_plus_contour_polygones.py
+++ DTM_plus_contour_polygones.py
@@ -2,10 +2,12 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 import rasterio as rio
+import rasterio.features
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 from pyproj import CRS
 import os
+import shapely.geometry as geom
 
 # Define the CRS, in this case British National Grid CRS (EPSG:27700)
 selected_crs = CRS.from_epsg(27700)
@@ -36,19 +38,20 @@ for file_name in files:
         # Define the path of the file
         file_path = os.path.normpath(os.path.join(folder_path, file_name))
 
-        with rio.open(file_name) as dataset:
+        with rio.open(file_path) as dataset:
             # Read the raster data as a numpy array
             data = dataset.read()
 
             # Create a mask to select pixels with values higher than the threshold
-            mask = data < flood_level
+            mask = data < flood_level_input
 
             # Generate shapes from the mask
-            shapes_generated = rio.shapes(mask, transform=dataset.transform)
+            shapes_generated = rasterio.features.shapes(mask, transform=dataset.transform)
+            #print(shapes_generated.HEAD)
 
             # Convert shapes to polygons
-            polygons = [rio.shape(poly) for poly, value in shapes_generated if value == 1]
-            print(polygons[0])
+            polygons = [geom.shape(poly) for poly, value in shapes_generated if value == 1]
+            # print(polygons[1])
 
             # Create a GeoDataFrame from the polygons
             gdf = gpd.GeoDataFrame(geometry=polygons, crs=dataset.crs)
@@ -56,34 +59,3 @@ for file_name in files:
             # Save the GeoDataFrame to a shapefile
             output_shapefile = os.path.normpath(os.path.join(folder_path, base_name + '.shp'))
             gdf.to_file(output_shapefile)
-
-
-
-
-
-
-
-
-# Setting a DatasetReader object for reading the dataset and its attributes
-with rio.open(asc_file_path) as dataset:
-    if dataset.crs is None:
-        # Ask the user to specify the Coordinate Reference System (CRS)
-        new_crs = input(f"CRS is not specified for '{dataset.name}'. Please specify a CRS (e.g., 'EPSG:27700'): ")
-        print(f"The specified crs for the file is {new_crs}")
-
-        # Update the dataset's profile with the new CRS
-        profile = dataset.profile
-        profile['crs'] = new_crs
-
-        # Read the data
-        data = dataset.read()
-
-        # Write the data with the updated profile
-        with rio.open('new_raster.asc', 'w', **profile) as dst:
-            dst= rio.write(data)
-    else:
-        print(f"CRS is specified for '{dataset.name}': {dataset.crs}")
-
-# function that reads the .asc fies that contain the DTM raster info and transform them into geodataframe.
-#dtm = dataset.read('crs':)
-#print(dtm.crs)
